c0dbd316c0a7934462348ee2eb802827bcdc6b59
ac92 <alfredc_1092@hotmail.com>
2017-09-16 03:14:46 -0700

    Implemented methods

--- app/src/main/java/com/example/android/popcorn/ui/MovieCollectionPagerAdapter.java
+++ app/src/main/java/com/example/android/popcorn/ui/MovieCollectionPagerAdapter.java
@@ -4,11 +4,17 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by alfredchang on 2017-09-16.
  */
 
 public class MovieCollectionPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragments = new ArrayList<>();
+    private List<String> mFragmentTitles = new ArrayList<>();
 
     public MovieCollectionPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -16,11 +22,24 @@ public class MovieCollectionPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return mFragments.get(position);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitles.get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mFragments.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragments.add(fragment);
+    }
+
+    public void addFragmentTitle(String title) {
+        mFragmentTitles.add(title);
     }
 }
