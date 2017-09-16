54ad0ca8ff8782f43f61271355a1a8ae42c721b2
ac92 <alfredc_1092@hotmail.com>
2017-09-16 11:19:20 -0700

    Added new fragment classes

--- app/src/main/java/com/example/android/popcorn/MainActivity.java
+++ app/src/main/java/com/example/android/popcorn/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//        setupViewPager();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -49,4 +52,18 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+//    private void setupViewPager() {
+//        MovieCollectionPagerAdapter mPagerAdapter = new MovieCollectionPagerAdapter(getSupportFragmentManager());
+//        addFragments(mPagerAdapter);
+//        addFragmentTitles(mPagerAdapter);
+//    }
+//
+//    private void addFragments(FragmentPagerAdapter pagerAdapter) {
+//
+//    }
+//
+//    private void addFragmentTitles(FragmentPagerAdapter pagerAdapter) {
+//
+//    }
 }
