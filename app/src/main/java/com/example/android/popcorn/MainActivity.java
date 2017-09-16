fba93ea80e29d7fabc4e2d7c3ca33fa80b102f77
ac92 <alfredc_1092@hotmail.com>
2017-09-16 12:02:25 -0700

    Added new fragment class; added tablayout widget

--- app/src/main/java/com/example/android/popcorn/MainActivity.java
+++ app/src/main/java/com/example/android/popcorn/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.popcorn;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.example.android.popcorn.fragments.CurrentFragment;
+import com.example.android.popcorn.fragments.FavouriteFragment;
 import com.example.android.popcorn.fragments.PopularFragment;
 import com.example.android.popcorn.fragments.TopFragment;
 import com.example.android.popcorn.ui.MovieCollectionPagerAdapter;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private final String POPULAR_MOVIES = "Popular";
     private final String TOP_MOVIES = "Top";
     private final String CURRENT_MOVIES = "Current";
+    private final String FAVOURITE_MOVIES = "Favourite";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -74,11 +80,13 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter.addFragment(new PopularFragment());
         pagerAdapter.addFragment(new TopFragment());
         pagerAdapter.addFragment(new CurrentFragment());
+        pagerAdapter.addFragment(new FavouriteFragment());
     }
 
     private void addFragmentTitles(MovieCollectionPagerAdapter pagerAdapter) {
         pagerAdapter.addFragmentTitle(POPULAR_MOVIES);
         pagerAdapter.addFragmentTitle(TOP_MOVIES);
         pagerAdapter.addFragmentTitle(CURRENT_MOVIES);
+        pagerAdapter.addFragmentTitle(FAVOURITE_MOVIES);
     }
 }
---
+++ app/src/main/java/com/example/android/popcorn/fragments/FavouriteFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.popcorn.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alfredchang on 2017-09-16.
+ */
+
+public class FavouriteFragment extends Fragment {
+}
