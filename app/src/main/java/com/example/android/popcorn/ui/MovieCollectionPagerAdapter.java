d01567d4aa47e52f9240f38784a7519ce448e1a7
ac92 <alfredc_1092@hotmail.com>
2017-09-16 03:05:24 -0700

    Added pager adapter class

---
+++ app/src/main/java/com/example/android/popcorn/ui/MovieCollectionPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.example.android.popcorn.ui;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by alfredchang on 2017-09-16.
+ */
+
+public class MovieCollectionPagerAdapter extends FragmentPagerAdapter {
+
+    public MovieCollectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
