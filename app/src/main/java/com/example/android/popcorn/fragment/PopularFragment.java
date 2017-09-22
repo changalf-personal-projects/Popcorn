c1c57949215f02aa4caef711a5416a0ee033c159
ac92 <alfredc_1092@hotmail.com>
2017-09-21 20:13:56 -0700

    Set up recycler view in fragment

--- app/src/main/java/com/example/android/popcorn/fragment/PopularFragment.java
+++ app/src/main/java/com/example/android/popcorn/fragment/PopularFragment.java
@@ -2,6 +2,8 @@ package com.example.android.popcorn.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +15,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.example.android.popcorn.BuildConfig;
 import com.example.android.popcorn.R;
+import com.example.android.popcorn.model.Movie;
 import com.example.android.popcorn.network.RequestQueueSingleton;
+import com.example.android.popcorn.ui.PosterRecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by alfredchang on 2017-09-16.
@@ -22,12 +32,27 @@ import com.example.android.popcorn.network.RequestQueueSingleton;
 public class PopularFragment extends Fragment {
 
     private final String LOG_TAG = PopularFragment.class.getSimpleName();
+    private final int NUM_COLUMNS_LAYOUT = 2;
+
+    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
+
+    private PosterRecyclerViewAdapter mRecyclerAdapter;
+    private List<Movie> mListOfMovies;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, rootView);
+
         fetchJsonData();
+
+        mListOfMovies = new ArrayList<>();
+        mRecyclerAdapter = new PosterRecyclerViewAdapter(mListOfMovies);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), NUM_COLUMNS_LAYOUT);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+
         return rootView;
     }
 
