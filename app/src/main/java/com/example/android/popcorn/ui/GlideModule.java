76f674e6e1f9c23748d28920fa1236c6fc5eccee
ac92 <alfredc_1092@hotmail.com>
2017-09-23 14:46:29 -0700

    Added class extending AppGlideModule to use Glide 4.1.1

---
+++ app/src/main/java/com/example/android/popcorn/ui/GlideModule.java
@@ -0,0 +1,10 @@
+package com.example.android.popcorn.ui;
+
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * This class is needed to fully access Glide 4.x.x.  This project is using
+ * Glide 4.1.1.
+ */
+public class GlideModule extends AppGlideModule {
+}
