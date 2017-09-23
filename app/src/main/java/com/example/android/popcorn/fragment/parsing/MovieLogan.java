2264b8fd81afee912d6219943a3e26d45e8ba8ce
ac92 <alfredc_1092@hotmail.com>
2017-09-23 02:40:20 -0700

    Added Jackson dependencies

--- app/src/main/java/com/example/android/popcorn/fragment/parsing/MovieLogan.java
+++ app/src/main/java/com/example/android/popcorn/fragment/parsing/MovieLogan.java
@@ -1,40 +1,10 @@
 package com.example.android.popcorn.fragment.parsing;
 
-import com.bluelinelabs.logansquare.annotation.JsonField;
-import com.bluelinelabs.logansquare.annotation.JsonObject;
-
-import java.util.List;
-
 /**
  * Used with LoganSquare.
  */
 
-@JsonObject
+
 public class MovieLogan {
 
-    @JsonField
-    private List<Results> results;
-
-    public List<Results> getResults() {
-        return results;
-    }
-
-    public void setResults(List<Results> results) {
-        this.results = results;
-    }
-
-    @JsonObject
-    public static class Results {
-
-        @JsonField(name = "poster_path")
-        private String posterPath;
-
-        public String getPosterPath() {
-            return posterPath;
-        }
-
-        public void setPosterPath(String posterPath) {
-            this.posterPath = posterPath;
-        }
-    }
 }
