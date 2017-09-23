d40a2ab549ca29d27e2b8ae02f2ec3291f1f3d3a
ac92 <alfredc_1092@hotmail.com>
2017-09-23 14:50:00 -0700

    Changed class name; added annotation

--- app/src/main/java/com/example/android/popcorn/ui/PosterGlideModule.java
+++ app/src/main/java/com/example/android/popcorn/ui/PosterGlideModule.java
@@ -1,10 +1,13 @@
 package com.example.android.popcorn.ui;
 
+import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
 /**
  * This class is needed to fully access Glide 4.x.x.  This project is using
  * Glide 4.1.1.
  */
-public class GlideModule extends AppGlideModule {
+
+@GlideModule
+public class PosterGlideModule extends AppGlideModule {
 }
