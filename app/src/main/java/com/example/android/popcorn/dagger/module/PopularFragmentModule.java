d6d1d89d8fa45a2276d7d8531cc3cf4e0b810aff
ac92 <alfredc_1092@hotmail.com>
2017-09-27 02:04:37 -0700

    Added module class

---
+++ app/src/main/java/com/example/android/popcorn/dagger/module/PopularFragmentModule.java
@@ -0,0 +1,11 @@
+package com.example.android.popcorn.dagger.module;
+
+import dagger.Module;
+
+/**
+ * Created by alfredchang on 2017-09-27.
+ */
+
+@Module
+public class PopularFragmentModule {
+}
