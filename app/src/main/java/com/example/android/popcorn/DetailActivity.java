927d18fff7b017cba8db7b506b8f9611b33e4a09
ac92 <alfredc_1092@hotmail.com>
2017-09-27 23:40:49 -0700

    Added detail class

---
+++ app/src/main/java/com/example/android/popcorn/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.popcorn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alfredchang on 2017-09-27.
+ */
+
+public class DetailActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
