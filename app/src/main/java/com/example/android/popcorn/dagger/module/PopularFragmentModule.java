e1799372a95b6599661dda6bf914565503603821
ac92 <alfredc_1092@hotmail.com>
2017-09-27 21:28:18 -0700

    Moved module class to another package

--- app/src/main/java/com/example/android/popcorn/dagger/module/PopularFragmentModule.java
+++ app/src/main/java/com/example/android/popcorn/dagger/module/PopularFragmentModule.java
@@ -1,6 +1,10 @@
 package com.example.android.popcorn.dagger.module;
 
+import com.example.android.popcorn.fragment.PopularFragment;
+import com.example.android.popcorn.ui.PosterRecyclerViewAdapter;
+
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by alfredchang on 2017-09-27.
@@ -8,4 +12,14 @@ import dagger.Module;
 
 @Module
 public class PopularFragmentModule {
+
+    @Provides
+    public PosterRecyclerViewAdapter provideRecyclerAdapter() {
+        return new PosterRecyclerViewAdapter(null);
+    }
+
+    @Provides
+    public PopularFragment providePopularFragment() {
+        return new PopularFragment();
+    }
 }
