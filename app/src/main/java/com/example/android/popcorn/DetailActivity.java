7fa6aa101f6b764224441adc3ece8684a9822244
ac92 <alfredc_1092@hotmail.com>
2017-09-28 00:01:58 -0700

    Implemented detail activity class

--- app/src/main/java/com/example/android/popcorn/DetailActivity.java
+++ app/src/main/java/com/example/android/popcorn/DetailActivity.java
@@ -1,16 +1,43 @@
 package com.example.android.popcorn;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.android.popcorn.fragment.DetailFragment;
 
 /**
  * Created by alfredchang on 2017-09-27.
  */
 
-public class DetailActivity extends Activity {
+public class DetailActivity extends FragmentActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container,
+                    new DetailFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.action_settings) {
+
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 }
---
+++ app/src/main/java/com/example/android/popcorn/fragment/DetailFragment.java
@@ -0,0 +1,14 @@
+package com.example.android.popcorn.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alfredchang on 2017-09-27.
+ */
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() {
+
+    }
+}
