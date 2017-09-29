618744bbc298a9936bf1b6a7620c4abc5709b5dc
ac92 <alfredc_1092@hotmail.com>
2017-09-29 00:15:49 -0700

    Added click listener interface

---
+++ app/src/main/java/com/example/android/popcorn/ui/OnMovieClickListener.java
@@ -0,0 +1,12 @@
+package com.example.android.popcorn.ui;
+
+import com.example.android.popcorn.model.Movie;
+
+/**
+ * Created by alfredchang on 2017-09-29.
+ */
+
+public interface OnMovieClickListener {
+
+    public void onMovieClick(Movie movie);
+}
