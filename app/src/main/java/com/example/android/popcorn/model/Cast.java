59b674bb11ac3d0ecacf3e30163d6065be899de5
ac92 <alfredc_1092@hotmail.com>
2017-10-03 00:39:09 -0700

    Implemented Parcelable

--- app/src/main/java/com/example/android/popcorn/model/Cast.java
+++ app/src/main/java/com/example/android/popcorn/model/Cast.java
@@ -1,13 +1,37 @@
 package com.example.android.popcorn.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by alfredchang on 2017-10-02.
  */
 
-public class Cast {
+public class Cast implements Parcelable {
 
     private String name;
     private String profilePath;
+
+    public Cast() {
+
+    }
+
+    protected Cast(Parcel in) {
+        name = in.readString();
+        profilePath = in.readString();
+    }
+
+    public static final Creator<Cast> CREATOR = new Creator<Cast>() {
+        @Override
+        public Cast createFromParcel(Parcel in) {
+            return new Cast(in);
+        }
+
+        @Override
+        public Cast[] newArray(int size) {
+            return new Cast[size];
+        }
+    };
 
     public String getName() {
         return name;
@@ -23,5 +47,16 @@ public class Cast {
 
     public void setProfilePath(String profilePath) {
         this.profilePath = profilePath;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(profilePath);
+    }
+
+    @Override
+    public int describeContents() {
+        return hashCode();
     }
 }
