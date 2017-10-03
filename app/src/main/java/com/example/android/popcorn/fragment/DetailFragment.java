ff5964a997b420ae1afc2a42f0f5df4732b23e43
ac92 <alfredc_1092@hotmail.com>
2017-10-02 19:08:11 -0700

    Switched detail activity and fragment layouts; bound views with Butterknife

--- app/src/main/java/com/example/android/popcorn/fragment/DetailFragment.java
+++ app/src/main/java/com/example/android/popcorn/fragment/DetailFragment.java
@@ -7,12 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.android.popcorn.R;
 import com.example.android.popcorn.Utilities;
 import com.example.android.popcorn.model.Movie;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by alfredchang on 2017-09-27.
@@ -23,11 +25,15 @@ public class DetailFragment extends Fragment {
     private final String LOG_TAG = DetailFragment.class.getSimpleName();
 
     @BindView(R.id.backdrop_poster) ImageView mBackdrop;
+    @BindView(R.id.movie_poster) ImageView mPoster;
+    @BindView(R.id.rating) TextView mRating;
+    @BindView(R.id.genres) TextView mGenres;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_detail_main, container, false);
+        ButterKnife.bind(this, rootView);
 
         fetchParcelable();
 
