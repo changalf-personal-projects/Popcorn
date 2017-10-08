96bbacab96669ecd923ec1c452082d60e8ffe4c9
ac92 <alfredc_1092@hotmail.com>
2017-10-08 03:39:44 -0700

    Added new (incomplete) fragment

--- app/src/main/java/com/example/android/popcorn/SingleCastMemberDetailActivity.java
+++ app/src/main/java/com/example/android/popcorn/SingleCastMemberDetailActivity.java
@@ -1,10 +1,24 @@
 package com.example.android.popcorn;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.popcorn.fragment.parsing.SingleCastDetailFragment;
 
 /**
  * Created by alfredchang on 2017-10-08.
  */
 
 public class SingleCastMemberDetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cast_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container,
+                    new SingleCastDetailFragment()).commit();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/android/popcorn/fragment/parsing/SingleCastDetailFragment.java
@@ -0,0 +1,11 @@
+package com.example.android.popcorn.fragment.parsing;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alfredchang on 2017-10-08.
+ */
+
+public class SingleCastDetailFragment extends Fragment {
+
+}
