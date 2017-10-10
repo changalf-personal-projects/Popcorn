116f7143cf79a6f8f8cabc80e939552c6e8271e7
ac92 <alfredc_1092@hotmail.com>
2017-10-10 00:51:07 -0700

    Added cast member model file

---
+++ app/src/main/java/com/example/android/popcorn/model/CastMember.java
@@ -0,0 +1,45 @@
+package com.example.android.popcorn.model;
+
+/**
+ * Created by alfredchang on 2017-10-10.
+ */
+
+public class CastMember {
+
+    private String birthday;
+    private String deathday;
+    private String biography;
+    private String birthplace;
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getDeathday() {
+        return deathday;
+    }
+
+    public void setDeathday(String deathday) {
+        this.deathday = deathday;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getBirthplace() {
+        return birthplace;
+    }
+
+    public void setBirthplace(String birthplace) {
+        this.birthplace = birthplace;
+    }
+}
