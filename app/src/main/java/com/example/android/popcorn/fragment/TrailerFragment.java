f72d9cc0ef0f205d72288156d0e4fe2347ce80c2
ac92 <alfredc_1092@hotmail.com>
2017-10-15 23:33:18 -0700

    Added onCreateView() and fragment layout

--- app/src/main/java/com/example/android/popcorn/fragment/TrailerFragment.java
+++ app/src/main/java/com/example/android/popcorn/fragment/TrailerFragment.java
@@ -1,10 +1,24 @@
 package com.example.android.popcorn.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.popcorn.R;
 
 /**
  * Created by alfredchang on 2017-10-15.
  */
 
 public class TrailerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_trailer_main, container, false);
+
+        return rootView;
+    }
 }
