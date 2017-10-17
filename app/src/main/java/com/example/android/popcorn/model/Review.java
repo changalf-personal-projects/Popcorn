0b55cc5ff0dd983116f96eb44fb32556ee7198b9
ac92 <alfredc_1092@hotmail.com>
2017-10-17 01:15:49 -0700

    Implemented Parcelable in review model

--- app/src/main/java/com/example/android/popcorn/model/Review.java
+++ app/src/main/java/com/example/android/popcorn/model/Review.java
@@ -1,14 +1,27 @@
 package com.example.android.popcorn.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by alfredchang on 2017-09-28.
  */
 
-public class Review {
+public class Review implements Parcelable {
 
     // TODO: Use "append_to_response" in TMDB.
 
     private String author;
+    private String content;
+
+    public Review() {
+
+    }
+
+    protected Review(Parcel in) {
+        author = in.readString();
+        content = in.readString();
+    }
 
     public String getAuthor() {
         return author;
@@ -26,5 +39,26 @@ public class Review {
         this.content = content;
     }
 
-    private String content;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(author);
+        dest.writeString(content);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
 }
