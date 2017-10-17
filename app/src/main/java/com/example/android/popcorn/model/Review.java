a184fb43efdbbed8fd5bc01a7277883dbfde7791
ac92 <alfredc_1092@hotmail.com>
2017-10-17 01:11:32 -0700

    Implemented review model

--- app/src/main/java/com/example/android/popcorn/model/Review.java
+++ app/src/main/java/com/example/android/popcorn/model/Review.java
@@ -7,4 +7,24 @@ package com.example.android.popcorn.model;
 public class Review {
 
     // TODO: Use "append_to_response" in TMDB.
+
+    private String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
 }
