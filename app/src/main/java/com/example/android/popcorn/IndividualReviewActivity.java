fcf0587e3c450c86d65c85e420c8f2992365358a
ac92 <alfredc_1092@hotmail.com>
2017-10-18 03:05:15 -0700

    Started new fragment that will show single review

---
+++ app/src/main/java/com/example/android/popcorn/IndividualReviewActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.popcorn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.popcorn.fragment.IndividualReviewFragment;
+
+/**
+ * Created by alfredchang on 2017-10-18.
+ */
+
+public class IndividualReviewActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container,
+                    new IndividualReviewFragment()).commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/popcorn/fragment/IndividualReviewFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.popcorn.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alfredchang on 2017-10-18.
+ */
+
+public class IndividualReviewFragment extends Fragment {
+}
