8b86cbc0a469aa773acb6549ede2e6aee1f03328
ac92 <alfredc_1092@hotmail.com>
2017-10-19 03:14:12 -0700

    Tried to set empty reviews message

--- app/src/main/java/com/example/android/popcorn/ui/review_recyclerview/ReviewRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/popcorn/ui/review_recyclerview/ReviewRecyclerViewAdapter.java
@@ -1,7 +1,9 @@
 package com.example.android.popcorn.ui.review_recyclerview;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,10 @@ import butterknife.ButterKnife;
 
 public class ReviewRecyclerViewAdapter extends RecyclerView.Adapter<ReviewRecyclerViewAdapter.ReviewViewHolder> {
 
+    private final String LOG_TAG = ReviewRecyclerViewAdapter.class.getSimpleName();
+
+    private int VIEW_TYPE_WITH_REVIEWS = 0;
+    private int VIEW_TYPE_WITHOUT_REVIEWS = 1;
     private final String NO_REVIEWS = "No reviews available.";
 
     private Context mContext;
@@ -34,11 +40,33 @@ public class ReviewRecyclerViewAdapter extends RecyclerView.Adapter<ReviewRecycl
     }
 
     @Override
+    public int getItemViewType(int position) {
+        if (mReviews.size() == 0) {
+            return VIEW_TYPE_WITHOUT_REVIEWS;
+        } else {
+            return VIEW_TYPE_WITH_REVIEWS;
+        }
+    }
+
+    @Override
     public ReviewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ReviewViewHolder reviewViewHolder = null;
+
+        if (viewType == VIEW_TYPE_WITHOUT_REVIEWS) {
+            reviewViewHolder = createViewHolder(R.layout.movie_review_empty, parent);
+        }
+
+        if (viewType == VIEW_TYPE_WITH_REVIEWS) {
+            reviewViewHolder = createViewHolder(R.layout.movie_review, parent);
+        }
+
+        return reviewViewHolder;
+    }
+
+    private ReviewViewHolder createViewHolder(int layoutId, ViewGroup parent) {
         mContext = parent.getContext();
-        int reviewLayoutId = R.layout.movie_review;
         LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-        View view = layoutInflater.inflate(reviewLayoutId, parent, false);
+        View view = layoutInflater.inflate(layoutId, parent, false);
         ReviewViewHolder reviewViewHolder = new ReviewViewHolder(view);
 
         return reviewViewHolder;
@@ -46,13 +74,18 @@ public class ReviewRecyclerViewAdapter extends RecyclerView.Adapter<ReviewRecycl
 
     @Override
     public void onBindViewHolder(ReviewViewHolder holder, int position) {
-        Review review = mReviews.get(position);
+        int viewType = holder.getItemViewType();
 
-        if (review != null) {
+        Log.v(LOG_TAG, "View type: " + viewType);
+
+        if (viewType == VIEW_TYPE_WITHOUT_REVIEWS) {
+            holder.mContent.setText(NO_REVIEWS);
+        }
+
+        if (viewType == VIEW_TYPE_WITH_REVIEWS) {
+            Review review = mReviews.get(position);
             onBindAuthor(review, holder);
             onBindContent(review, holder);
-        } else {
-            holder.mContent.setText(NO_REVIEWS);
         }
     }
 
@@ -71,8 +104,8 @@ public class ReviewRecyclerViewAdapter extends RecyclerView.Adapter<ReviewRecycl
 
     public class ReviewViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        @BindView(R.id.author) TextView mAuthor;
-        @BindView(R.id.content) TextView mContent;
+        @Nullable @BindView(R.id.author) TextView mAuthor;
+        @Nullable @BindView(R.id.content) TextView mContent;
 
         public ReviewViewHolder(View itemView) {
             super(itemView);
