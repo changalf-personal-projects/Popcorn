e806fd553da587662ce90a0104659a30ba7de89b
ac92 <alfredc_1092@hotmail.com>
2017-10-21 03:48:09 -0700

    Used Butterknife to bind image view

--- app/src/main/java/com/example/android/popcorn/fragment/IndividualReviewFragment.java
+++ app/src/main/java/com/example/android/popcorn/fragment/IndividualReviewFragment.java
@@ -5,8 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.android.popcorn.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by alfredchang on 2017-10-18.
@@ -14,10 +18,14 @@ import com.example.android.popcorn.R;
 
 public class IndividualReviewFragment extends Fragment {
 
+    @BindView(R.id.movie_poster) ImageView mMoviePoster;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         super.onCreateView(inflater, parent, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_review_main, parent, false);
+        ButterKnife.bind(this, rootView);
+
 
         return rootView;
     }
