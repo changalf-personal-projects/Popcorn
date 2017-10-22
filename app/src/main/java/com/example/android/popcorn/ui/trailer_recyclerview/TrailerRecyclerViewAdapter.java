a32676c623240d059a483278761589d999e4eb0e
ac92 <alfredc_1092@hotmail.com>
2017-10-22 03:49:22 -0700

    Initialized field variables

--- app/src/main/java/com/example/android/popcorn/ui/trailer_recyclerview/TrailerRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/popcorn/ui/trailer_recyclerview/TrailerRecyclerViewAdapter.java
@@ -1,15 +1,28 @@
 package com.example.android.popcorn.ui.trailer_recyclerview;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.android.popcorn.model.Trailer;
+
+import java.util.List;
 
 /**
  * Created by alfredchang on 2017-10-22.
  */
 
 public class TrailerRecyclerViewAdapter extends RecyclerView.Adapter<TrailerRecyclerViewAdapter.TrailerViewHolder> {
+
+    private Context mContext;
+    private List<Trailer> mTrailers;
+
+    public TrailerRecyclerViewAdapter(Context context, List<Trailer> trailers) {
+        mContext = context;
+        mTrailers = trailers;
+    }
 
     @Override
     public TrailerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
