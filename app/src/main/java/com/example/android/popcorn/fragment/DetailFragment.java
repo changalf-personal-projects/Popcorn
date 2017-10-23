2b39bb5fd30e93363b008e880fbcb5d89d08b33f
ac92 <alfredc_1092@hotmail.com>
2017-10-22 22:59:06 -0700

    Added 'snapping' effect to trailers

--- app/src/main/java/com/example/android/popcorn/fragment/DetailFragment.java
+++ app/src/main/java/com/example/android/popcorn/fragment/DetailFragment.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -151,6 +153,11 @@ public class DetailFragment extends Fragment implements OnCastMemberClickListene
 
     private void attachToTrailerAdapter(Movie movie) {
         mTrailerRecyclerAdapter = new TrailerRecyclerViewAdapter(getActivity(), movie.getTrailers());
+
+        // Achieve the "snapping" effect.
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(mTrailerRecyclerView);
+
         mTrailerRecyclerView.setAdapter(mTrailerRecyclerAdapter);
     }
 
