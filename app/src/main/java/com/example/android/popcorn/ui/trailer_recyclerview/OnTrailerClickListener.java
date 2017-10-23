a7fc577cfebf0ce158f3209a177dd60d1d5bb4fd
ac92 <alfredc_1092@hotmail.com>
2017-10-23 04:06:40 -0700

    Added trailer click listener interface

---
+++ app/src/main/java/com/example/android/popcorn/ui/trailer_recyclerview/OnTrailerClickListener.java
@@ -0,0 +1,12 @@
+package com.example.android.popcorn.ui.trailer_recyclerview;
+
+import com.example.android.popcorn.model.Trailer;
+
+/**
+ * Created by alfredchang on 2017-10-23.
+ */
+
+public interface OnTrailerClickListener {
+
+    void onClick(Trailer trailer);
+}
