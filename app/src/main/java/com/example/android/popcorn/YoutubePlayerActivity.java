4f4091da2006dc38a74839ce5dba5ce47fe786fd
ac92 <alfredc_1092@hotmail.com>
2017-10-23 21:00:55 -0700

    Implemented method to play video upon successful load

--- app/src/main/java/com/example/android/popcorn/YoutubePlayerActivity.java
+++ app/src/main/java/com/example/android/popcorn/YoutubePlayerActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.popcorn;
 
+import android.content.Intent;
+
+import com.example.android.popcorn.model.Trailer;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
 
 /**
  * Created by alfredchang on 2017-10-23.
@@ -11,12 +15,26 @@ import com.google.android.youtube.player.YouTubePlayer;
 public class YoutubePlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-
+    public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer, boolean shouldContinue) {
+        Trailer trailer = getVideoIdFromParcelable();
+        String trailerId = getTrailerIdFromTrailer(trailer);
+        if (!shouldContinue) {
+            youTubePlayer.loadVideo(trailerId);
+        }
     }
 
     @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
 
+    }
+
+    private Trailer getVideoIdFromParcelable() {
+        Intent playTrailerIntent = getIntent();
+        Trailer trailer = playTrailerIntent.getParcelableExtra(Utilities.PARCELABLE_TRAILER_KEY);
+        return trailer;
+    }
+
+    private String getTrailerIdFromTrailer(Trailer trailer) {
+        return trailer.getKey();
     }
 }
