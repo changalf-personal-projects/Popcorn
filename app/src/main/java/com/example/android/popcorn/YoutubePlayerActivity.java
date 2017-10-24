039b9feb6f07662abde609a6051fc52262ce33ba
ac92 <alfredc_1092@hotmail.com>
2017-10-23 21:12:16 -0700

    Added layout for youtube activity

--- app/src/main/java/com/example/android/popcorn/YoutubePlayerActivity.java
+++ app/src/main/java/com/example/android/popcorn/YoutubePlayerActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.popcorn;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.android.popcorn.model.Trailer;
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -14,11 +17,21 @@ import com.google.android.youtube.player.YouTubePlayer.Provider;
 
 public class YoutubePlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
+    private final String LOG_TAG = YoutubePlayerActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_player);
+    }
+
     @Override
     public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer, boolean shouldContinue) {
         Trailer trailer = getVideoIdFromParcelable();
         String trailerId = getTrailerIdFromTrailer(trailer);
+        Log.v(LOG_TAG, "Trailer id: " + trailerId);
         if (!shouldContinue) {
+            Toast.makeText(this, "Playing video...", Toast.LENGTH_SHORT).show();
             youTubePlayer.loadVideo(trailerId);
         }
     }
