a13ab8aa9d401f3169e13ba0122f21a4acd6454a
ac92 <alfredc_1092@hotmail.com>
2017-10-28 00:44:02 -0700

    Added ReviewActivity class

--- app/src/main/java/com/example/android/popcorn/activity/ReviewActivity.java
+++ app/src/main/java/com/example/android/popcorn/activity/ReviewActivity.java
@@ -1,10 +1,25 @@
 package com.example.android.popcorn.activity;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.popcorn.R;
+import com.example.android.popcorn.fragment.parsing.ReviewFragment;
+
 /**
  * Created by alfredchang on 2017-10-28.
  */
 
-public class ReviewActivity {
+public class ReviewActivity extends AppCompatActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_main);
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container,
+                    new ReviewFragment()).commit();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/android/popcorn/fragment/parsing/ReviewFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.popcorn.fragment.parsing;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alfredchang on 2017-10-28.
+ */
+
+public class ReviewFragment extends Fragment {
+}
