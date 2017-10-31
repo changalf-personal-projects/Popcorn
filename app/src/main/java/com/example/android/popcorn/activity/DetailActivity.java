4ddce902f63f2ab8e73fdaee16202607a9ee29cc
ac92 <alfredc_1092@hotmail.com>
2017-10-31 02:48:58 -0700

    Added transparent appbar layout

--- app/src/main/java/com/example/android/popcorn/activity/DetailActivity.java
+++ app/src/main/java/com/example/android/popcorn/activity/DetailActivity.java
@@ -28,16 +28,16 @@ public class DetailActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_main);
-        setSupportActionBar(mToolbar);
+//        setSupportActionBar(mToolbar);
 
         // Adjusting toolbar.
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.transparent_toolbar));
+//            getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.transparent_layout));
 //        }
 //        Drawable backArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
 //        backArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
 //        getSupportActionBar().setHomeAsUpIndicator(backArrow);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         if (savedInstanceState == null) {
