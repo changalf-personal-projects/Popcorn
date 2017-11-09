bae4ab6a70550fca8e3db2a00496a9790cd99391
ac92 <alfredc_1092@hotmail.com>
2017-11-08 21:07:02 -0800

    Implemented pager adapter class

---
+++ app/src/main/java/com/example/android/popcorn/ui/DetailTabsPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.android.popcorn.ui;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alfredchang on 2017-11-08.
+ */
+
+public class DetailTabsPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragments = new ArrayList<>();
+    private List<String> mFragmentTitles = new ArrayList<>();
+
+    public DetailTabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public String getPageTitle(int position) {
+        return mFragmentTitles.get(position);
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragments.add(fragment);
+    }
+
+    public void addFragmentTitle(String title) {
+        mFragmentTitles.add(title);
+    }
+}
