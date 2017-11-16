24418a7d862187dc223375bef8ffa69227fef650
ac92 <alfredc_1092@hotmail.com>
2017-11-15 19:08:52 -0800

    Commit

--- app/src/main/java/com/example/android/popcorn/activity/DetailActivity.java
+++ app/src/main/java/com/example/android/popcorn/activity/DetailActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -54,8 +57,8 @@ public class DetailActivity extends AppCompatActivity {
     ViewPager mViewPager;
     @BindView(R.id.tmdb_branding)
     ImageView tmdbBranding;
-//    @BindView(R.id.favourite_fab)
-//    FloatingActionButton favouriteButton;
+    @BindView(R.id.favourite_fab)
+    FloatingActionButton favouriteButton;
 
     // Movie info.
     @BindView(R.id.title)
@@ -89,13 +92,13 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void initFab() {
-//        favouriteButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        favouriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
