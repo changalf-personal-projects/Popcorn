61aacbf33314b1e302e8ec749ae61b2dc3a41a6b
ac92 <alfredc_1092@hotmail.com>
2017-11-20 20:17:42 -0800

    Saved query to bundle so it can be passed to SearchResultsFragment.java

--- app/src/main/java/com/example/android/popcorn/activity/SearchResultsActivity.java
+++ app/src/main/java/com/example/android/popcorn/activity/SearchResultsActivity.java
@@ -2,7 +2,9 @@ package com.example.android.popcorn.activity;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.android.popcorn.R;
+import com.example.android.popcorn.fragment.SearchResultsFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,9 +33,26 @@ public class SearchResultsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
 
+        Fragment searchResultsFragment = saveQueryToBundle();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.search_results_container,
+                    searchResultsFragment).commit();
+        }
+    }
+
+    private Fragment saveQueryToBundle() {
+        String query = getQueryFromIntent();
+        Fragment searchResultsFragment = new SearchResultsFragment();
+        Bundle bundle = new Bundle();
         // Coupled the search key because it will always have to be identical to work.
-        String query = getIntent().getStringExtra(SearchQueryActivity.SEARCH_KEY);
-        searchForResultsWithQuery(query);
+        bundle.putString(SearchQueryActivity.SEARCH_KEY, query);
+        searchResultsFragment.setArguments(bundle);
+        return searchResultsFragment;
+    }
+
+    private String getQueryFromIntent() {
+        return getIntent().getStringExtra(SearchQueryActivity.SEARCH_KEY);
     }
 
     @Override
@@ -44,6 +64,20 @@ public class SearchResultsActivity extends AppCompatActivity {
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.search_icon).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                resetSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Unused here.
+                return false;
+            }
+        });
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -59,8 +93,10 @@ public class SearchResultsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void searchForResultsWithQuery(String query) {
-
+    private void resetSearch(String query) {
+        Intent searchIntent = new Intent(this, SearchResultsActivity.class);
+        searchIntent.putExtra(SearchQueryActivity.SEARCH_KEY, query);
+        startActivity(searchIntent);
     }
 
 }
---
+++ app/src/main/java/com/example/android/popcorn/fragment/SearchResultsFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.popcorn.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alfredchang on 2017-11-20.
+ */
+
+public class SearchResultsFragment extends Fragment {
+}
