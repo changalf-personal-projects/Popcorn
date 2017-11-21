373da1476fe9a7afe892e94f0c65327b648edb06
ac92 <alfredc_1092@hotmail.com>
2017-11-20 18:59:48 -0800

    Created layout file for SearchResultsActivity.java

--- app/src/main/java/com/example/android/popcorn/activity/SearchResultsActivity.java
+++ app/src/main/java/com/example/android/popcorn/activity/SearchResultsActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.popcorn.activity;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by alfredchang on 2017-11-20.
  */
 
-public class SearchResultsActivity {
+public class SearchResultsActivity extends AppCompatActivity {
+
 }
