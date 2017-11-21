afc9a3bec7ad8f42677dd02a794091fa41c5995c
ac92 <alfredc_1092@hotmail.com>
2017-11-20 19:19:48 -0800

    Added SearchResultsActivity.java into manifest file; added overidden methods in SearchResultsActivity.java

--- app/src/main/java/com/example/android/popcorn/activity/SearchResultsActivity.java
+++ app/src/main/java/com/example/android/popcorn/activity/SearchResultsActivity.java
@@ -1,11 +1,49 @@
 package com.example.android.popcorn.activity;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.android.popcorn.R;
 
 /**
  * Created by alfredchang on 2017-11-20.
  */
 
 public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Source: https://developer.android.com/training/search/setup.html.
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_icon).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
