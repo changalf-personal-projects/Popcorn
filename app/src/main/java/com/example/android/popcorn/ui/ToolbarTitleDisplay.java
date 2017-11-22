9dbe839b05965c944d6c40e86052d24d61737c60
ac92 <alfredc_1092@hotmail.com>
2017-11-21 23:20:51 -0800

    Removed method

--- app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
+++ app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
@@ -1,21 +1,9 @@
 package com.example.android.popcorn.ui;
 
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
-
 /**
  * Created by alfredchang on 2017-11-15.
  */
 
 public class ToolbarTitleDisplay {
 
-    public static void displayTitleUponCollapse(AppBarLayout appBarLayout,
-                                                CollapsingToolbarLayout collapsingToolbarLayout) {
-        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-
-            }
-        });
-    }
 }
