d155938a6df0c90cd197124dcf9e0e3d7390a54b
ac92 <alfredc_1092@hotmail.com>
2017-11-21 23:25:11 -0800

    Extended CircleImageView.java; updated dependencies

--- app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
+++ app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
@@ -1,9 +1,13 @@
 package com.example.android.popcorn.ui;
 
+import android.support.design.widget.CoordinatorLayout;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 /**
  * Created by alfredchang on 2017-11-15.
  */
 
-public class ToolbarTitleDisplay {
+public class ToolbarTitleDisplay extends CoordinatorLayout.Behavior<CircleImageView> {
 
 }
