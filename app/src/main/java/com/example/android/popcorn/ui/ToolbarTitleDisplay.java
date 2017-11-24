effb522e0d3bcd7484f2db2cb0de53f76d6db3e2
ac92 <alfredc_1092@hotmail.com>
2017-11-23 19:14:12 -0800

    Added stub methods

--- app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
+++ app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
@@ -1,13 +1,30 @@
 package com.example.android.popcorn.ui;
 
 import android.support.design.widget.CoordinatorLayout;
+import android.view.View;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
- * Created by alfredchang on 2017-11-15.
+ * ImageView for movie poster when toolbar is collapsed and profile pictures of cast members.
  */
 
 public class ToolbarTitleDisplay extends CoordinatorLayout.Behavior<CircleImageView> {
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout coordinatorLayout, CircleImageView child,
+                                   View dependency) {
+        return true;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout coordinatorLayout, CircleImageView avatar,
+                                          View dependency) {
+        return true;
+    }
+
+    private void modifyAvatarDependingOnDependencyState(CircleImageView avatar, View dependency) {
+
+    }
 
 }
