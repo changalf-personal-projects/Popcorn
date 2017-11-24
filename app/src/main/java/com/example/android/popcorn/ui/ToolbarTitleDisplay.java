478ebfca01529da68a9240d9f0e3a865dc928bab
ac92 <alfredc_1092@hotmail.com>
2017-11-23 19:32:02 -0800

    Completed methods for CircularImageView behaviour

--- app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
+++ app/src/main/java/com/example/android/popcorn/ui/ToolbarTitleDisplay.java
@@ -2,29 +2,36 @@ package com.example.android.popcorn.ui;
 
 import android.support.design.widget.CoordinatorLayout;
 import android.view.View;
+import android.widget.Toolbar;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
- * ImageView for movie poster when toolbar is collapsed and profile pictures of cast members.
+ * Customizinig the animation states for a circular ImageView.  The view will be used when toolbar
+ * collapses.
  */
 
 public class ToolbarTitleDisplay extends CoordinatorLayout.Behavior<CircleImageView> {
 
+    private final int X_POS = 20;
+    private final int Y_POS = 20;
+
     @Override
     public boolean layoutDependsOn(CoordinatorLayout coordinatorLayout, CircleImageView child,
                                    View dependency) {
-        return true;
+        return dependency instanceof Toolbar;
     }
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout coordinatorLayout, CircleImageView avatar,
                                           View dependency) {
+        modifyAvatarDependingOnDependencyState(avatar, dependency);
         return true;
     }
 
     private void modifyAvatarDependingOnDependencyState(CircleImageView avatar, View dependency) {
-
+        avatar.setX(X_POS);
+        avatar.setY(Y_POS);
     }
 
 }
