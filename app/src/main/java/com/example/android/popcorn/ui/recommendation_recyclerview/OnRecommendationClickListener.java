64881a20c0d00a24899fd25a1870d857409db997
ac92 <alfredc_1092@hotmail.com>
2017-12-05 13:07:49 -0800

    Added recommended movie recyclerview item

---
+++ app/src/main/java/com/example/android/popcorn/ui/recommendation_recyclerview/OnRecommendationClickListener.java
@@ -0,0 +1,8 @@
+package com.example.android.popcorn.ui.recommendation_recyclerview;
+
+/**
+ * Created by alfredchang on 2017-12-05.
+ */
+
+public interface OnRecommendationClickListener {
+}
---
+++ app/src/main/java/com/example/android/popcorn/ui/recommendation_recyclerview/RecommendationRecyclerViewAdapter.java
@@ -0,0 +1,18 @@
+package com.example.android.popcorn.ui.recommendation_recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by alfredchang on 2017-12-05.
+ */
+
+public class RecommendationRecyclerViewAdapter {
+
+    public class RecommendationViewHolder extends RecyclerView.ViewHolder {
+
+        public RecommendationViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
