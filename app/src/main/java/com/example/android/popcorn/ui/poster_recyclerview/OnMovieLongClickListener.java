ed2f3e5f785a1e6be33840a22c682029314c6f3b
ac92 <alfredc_1092@hotmail.com>
2019-02-24 17:12:48 -0800

    Created new interface to handle long clicks of movie posters

---
+++ app/src/main/java/com/example/android/popcorn/ui/poster_recyclerview/OnMovieLongClickListener.java
@@ -0,0 +1,8 @@
+package com.example.android.popcorn.ui.poster_recyclerview;
+
+import com.example.android.popcorn.model.Movie;
+
+public interface OnMovieLongClickListener {
+
+    void onMovieLongClickListener(Movie movie);
+}
