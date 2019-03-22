608f72e1a046c2e1894e7840830af625f6cff85f
ac92 <alfredc_1092@hotmail.com>
2019-03-21 22:35:54 -0700

    Removed entire navigation function

--- app/src/main/java/com/example/android/popcorn/MainActivity.java
+++ app/src/main/java/com/example/android/popcorn/MainActivity.java
@@ -4,13 +4,8 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -30,7 +25,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int PAGES_TO_RETAIN = 2;
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private final int PAGES_TO_RETAIN = 1;
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
@@ -38,10 +35,6 @@ public class MainActivity extends AppCompatActivity {
     ViewPager mViewPager;
     @BindView(R.id.tab_layout)
     TabLayout mTabLayout;
-    @BindView(R.id.drawer_layout)
-    DrawerLayout mDrawerLayout;
-    @BindView(R.id.navigation_view)
-    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +43,8 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
-        setupFragmentReplacement();
-        setupNavigationDrawer();
         setupViewPager();
         mTabLayout.setupWithViewPager(mViewPager);
     }
@@ -77,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         SearchView searchView = (SearchView) menu.findItem(R.id.search_icon).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
             @Override
             public boolean onQueryTextSubmit(String query) {
                 startSearch(query);
@@ -97,41 +87,19 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        mDrawerLayout.openDrawer(GravityCompat.START);
+        switch (item.getItemId()) {
+            case R.id.action_sort :
+
+            case R.id.action_settings :
+
+            case R.id.action_feedback :
+
+            case R.id.action_about :
+
+            default :
+        }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void setupFragmentReplacement() {
-        getSupportFragmentManager().beginTransaction().replace(R.id.content_main_frame_layout,
-                new PopularFragment()).commit();
-    }
-
-    private void setupNavigationDrawer() {
-        mNavigationView.setNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.category_popular:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.content_main_frame_layout,
-                                new PopularFragment()).commit();
-                        break;
-
-                    case R.id.category_top:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.content_main, new TopFragment())
-                                .commit();
-                        break;
-
-                    default:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.content_main, new PopularFragment())
-                                .commit();
-                }
-
-                mDrawerLayout.closeDrawers();
-                return true;
-            }
-        });
     }
 
     private void setupViewPager() {
@@ -155,4 +123,5 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter.addFragmentTitle(TabTitles.CURRENT_MOVIES);
         pagerAdapter.addFragmentTitle(TabTitles.FAVOURITE_MOVIES);
     }
+
 }
