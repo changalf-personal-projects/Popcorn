67f11dcf45c497aba7633ddce159e6250d449d82
ac92 <alfredc_1092@hotmail.com>
2019-03-22 22:51:40 -0700

    Complete layout of alert dialog

--- app/src/main/java/com/example/android/popcorn/fragment/SortByDialogFragment.java
+++ app/src/main/java/com/example/android/popcorn/fragment/SortByDialogFragment.java
@@ -2,6 +2,8 @@ package com.example.android.popcorn.fragment;
 
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
@@ -9,12 +11,28 @@ import com.example.android.popcorn.R;
 
 public class SortByDialogFragment extends DialogFragment {
 
+    private static final String LOG_TAG = SortByDialogFragment.class.getSimpleName();
+
+    private final int NO_CHECKED_ITEM = -1;
+    private int mSelectedChoice;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Builder alertDialogBuilder = new Builder(getActivity(), android.R.style.Theme_Material_Dialog_Alert);
-        alertDialogBuilder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+        alertDialogBuilder.setTitle(R.string.dialog_title).setSingleChoiceItems(
+                R.array.sort_by_choices, NO_CHECKED_ITEM, new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        mSelectedChoice = i;
+                        dialogInterface.dismiss();
+                    }
+                });
 
         return alertDialogBuilder.create();
+    }
+
+    public int getSelectedChoice() {
+        return mSelectedChoice;
     }
 
 }
