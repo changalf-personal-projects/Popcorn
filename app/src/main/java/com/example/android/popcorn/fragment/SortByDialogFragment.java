6cd8ed437ee7425fa5df8a505771a0f18a46c6bc
ac92 <alfredc_1092@hotmail.com>
2019-03-22 19:16:08 -0700

    Added dummy title and dummy message to dialog

--- app/src/main/java/com/example/android/popcorn/fragment/SortByDialogFragment.java
+++ app/src/main/java/com/example/android/popcorn/fragment/SortByDialogFragment.java
@@ -5,11 +5,15 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
+import com.example.android.popcorn.R;
+
 public class SortByDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        alertDialogBuilder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+
         return alertDialogBuilder.create();
     }
 
