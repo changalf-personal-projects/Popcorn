24e78b245ae483615a06c7aceef26c565af8899d
ac92 <alfredc_1092@hotmail.com>
2019-03-25 22:57:38 -0700

    Commit

--- app/src/main/java/com/example/android/popcorn/fragment/DialogFragment/DialogComparator.java
+++ app/src/main/java/com/example/android/popcorn/fragment/DialogFragment/DialogComparator.java
@@ -1,7 +1,5 @@
 package com.example.android.popcorn.fragment.DialogFragment;
 
-import android.util.Log;
-
 import com.example.android.popcorn.model.Movie;
 
 import java.util.Comparator;
@@ -13,8 +11,6 @@ public class DialogComparator {
     public static Comparator<Movie> BestToWorstComparator = new Comparator<Movie>() {
         @Override
         public int compare(Movie movie, Movie t1) {
-            Log.d(LOG_TAG, "First movie: " + movie.getTitle() + "  Second movie: " + t1.getTitle());
-            Log.d(LOG_TAG, "Final number: " + (int) ((Double.parseDouble(movie.getRating()) - Double.parseDouble(t1.getRating())) * 10.0));
             return (int) ((Double.parseDouble(t1.getRating()) - Double.parseDouble(movie.getRating())) * 10.0);
         }
     };
